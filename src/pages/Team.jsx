60222126f446bcf158ca30d44017065b81f278c1
azrauygul <azrauygul112@gmail.com>
2025-03-19 00:32:54 +0300

    cards
    3x3

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Nav } from "../Components/Nav"
 import { Welcome } from "../sections/Welcome";
-import foto1 from "../assets/angry_bird.png"
+import foto1 from "../assets/angry_bird.png";
+import foto2 from "../assets/angry_bird.png";
+import foto3 from "../assets/angry_bird.png";
+import foto4 from "../assets/angry_bird.png";
+import foto5 from "../assets/angry_bird.png";
+import foto6 from "../assets/angry_bird.png";
+import foto7 from "../assets/angry_bird.png";
+import foto8 from "../assets/angry_bird.png";
+import foto9 from "../assets/angry_bird.png";
+
+
 
 import {FlipCard} from '../Components/FlipCard'
 
@@ -21,11 +31,23 @@ function Team() {
                         <Welcome/>
             </section>
 
+            <section className="grid grid-cols-3 gap-4 p-8">
+              <FlipCard foto={foto1}></FlipCard>
+              <FlipCard foto={foto2}></FlipCard>
+              <FlipCard foto={foto3}></FlipCard>
+              <FlipCard foto={foto4}></FlipCard>
+              <FlipCard foto={foto5}></FlipCard>
+              <FlipCard foto={foto6}></FlipCard>
+              <FlipCard foto={foto7}></FlipCard>
+              <FlipCard foto={foto8}></FlipCard>
+              <FlipCard foto={foto9}></FlipCard>
+            </section>
             
-        <FlipCard foto={foto1}></FlipCard>
+        
              
     </main>
-  )
+
+  );
 }
 
 export default Team
